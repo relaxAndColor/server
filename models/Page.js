7498ede8b31660c6da2fdbdbd10d78e0a6d25d8d
Whitney Harvey <wmelrose01@gmail.com>
2016-03-08 09:41:52 -0800

    add Page and Gallery models

---
+++ models/Page.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+//holds unedited svg images
+
+const Page = new Schema({
+  title: {
+    type: String,
+    require: true
+  },
+  category: {
+    type: String,
+    require: true
+  },
+  svg: {
+    type: String,
+    require: true
+  },
+  user: {type: mongoose.Schema.Types.ObjectId, ref: 'User'}
+});
+
+module.exports = mongoose.model('Page', Page);
